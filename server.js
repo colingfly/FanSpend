4aef3464f91bee6ff143fca2d3b4fdae28ea624c
colingly <colingfly@gmail.com>
2025-11-24 21:21:16 -0500

    Switch to mysql2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const url = require('url');
 const session = require('express-session');
 const path = require('path');
